36f22801db631eeb2b8cc1f1a1989f176d8e64c3
Hoang Trinh Huy <piavghoang@gmail.com>
2015-05-16 07:07:22 +0700

    Sửa GlobalTpl

--- www/js/global-template.js
+++ www/js/global-template.js
@@ -1,104 +1,112 @@
 angular.module('globaltpl', [])
 
-        .factory('GlobalTpl', function ($http, $ionicLoading, $ionicPopup) {
+    .factory('GlobalTpl', function ($http, $ionicLoading, $ionicPopup) {
 
-            function showLoading() {
-                $ionicLoading.show({
-                    template: 'Vui lòng chờ ...'
-                });
-            }
+        function showLoading() {
+            $ionicLoading.show({
+                template: 'Vui lòng chờ ...'
+            });
+        }
 
-            function hideLoading() {
-                $ionicLoading.hide();
-            }
+        function hideLoading() {
+            $ionicLoading.hide();
+        }
 
-            function showAlert(options) {
-                $ionicPopup.alert({
-                    title: (options.title) ? options.title : 'Thông báo',
-                    template: '<div class= "text-center">' + options.template + '</div>'
-                });
-            }
-            ;
+        function showAlert(options) {
+            $ionicPopup.alert({
+                title: (options.title) ? options.title : 'Thông báo',
+                template: '<div class= "text-center">' + options.template + '</div>'
+            });
+        }
+        ;
 
-            function _errorAlert() {
-                $ionicPopup.alert({
-                    title: 'Lỗi kết nối',
-                    template: 'Vui lòng kiểm tra lại kết nối. Ứng dụng sẽ tự động thoát!'
-                }).then(function(res) {
-					navigator.app.exitApp();
-				});
-				}
+        function _errorAlert() {
+            $ionicPopup.alert({
+                title: 'LỖI KẾT NỐI',
+                template: '<div class="only-text">Không có kết nối hoặc kết nối bị lỗi!</div>'
+            });
+            hideLoading();
+        }
 
-            function request(options, callback, final) {
+        function _errorAlertAndQuit() {
+            $ionicPopup.alert({
+                title: 'Lỗi kết nối',
+                template: 'Vui lòng kiểm tra lại kết nối. Ứng dụng sẽ tự động thoát!'
+            }).then(function (res) {
+                navigator.app.exitApp();
+            });
+        }
 
-                var config = {
-                    timeout: 15000,
-					header :{
-						'Content-Type': undefined
-					}
-                };
+        function request(options, callback, final) {
 
-                var isAlert = (options.showAlert) ? true : options.showAlert;
-
-                if (options.showLoad) {
-                    showLoading();
+            var config = {
+                timeout: 15000,
+                header: {
+                    'Content-Type': undefined
                 }
-
-                if (options.method === 'post') {
-                    $http.post(options.url, options.data, config).
-                            success(function (response) {
-
-                                if (response) {
-                                    // Execute the callback
-                                    if ((typeof callback !== 'undefined') && (typeof callback === 'function')) {
-                                        callback(response);
-                                    }
-                                }
-
-                            }).
-                            error(function () {
-                                if (isAlert)
-                                    _errorAlert();
-                            }).
-                            finally(function () {
-                                hideLoading();
-                                if ((typeof final !== 'undefined') && (typeof final === 'function')) {
-                                    final();
-                                }
-                            });
-                    ;
-
-                } else if ((options.method === 'get') || (options.method === 'delete')) {
-                    var method = $http[options.method];
-                    method(options.url, config).
-                            success(function (response) {
-                                // Implement callback
-                                if ((typeof callback !== 'undefined') && (typeof callback === 'function')) {
-                                    callback(response);
-                                }
-
-                            }).
-                            error(function () {
-                                if (isAlert)
-                                    _errorAlert();
-                            }).
-                            finally(function () {
-                                if (options.showLoad) {
-                                    hideLoading();
-                                }
-
-                                if ((typeof final !== 'undefined') && (typeof final === 'function')) {
-                                    final();
-                                }
-                            });
-                }
-            }
-
-            return {
-                showLoading: showLoading,
-                hideLoading: hideLoading,
-                request: request,
-                showAlert: showAlert
             };
 
-        });
+            var isAlert = (options.showAlert) ? true : options.showAlert;
+
+            if (options.showLoad) {
+                showLoading();
+            }
+
+            if (options.method === 'post') {
+                $http.post(options.url, options.data, config).
+                    success(function (response) {
+
+                        if (response) {
+                            // Execute the callback
+                            if ((typeof callback !== 'undefined') && (typeof callback === 'function')) {
+                                callback(response);
+                            }
+                        }
+
+                    }).
+                    error(function () {
+                        if (isAlert)
+                            _errorAlert();
+                    }).
+                    finally(function () {
+                        hideLoading();
+                        if ((typeof final !== 'undefined') && (typeof final === 'function')) {
+                            final();
+                        }
+                    });
+                ;
+
+            } else if ((options.method === 'get') || (options.method === 'delete')) {
+                var method = $http[options.method];
+                method(options.url, config).
+                    success(function (response) {
+                        // Implement callback
+                        if ((typeof callback !== 'undefined') && (typeof callback === 'function')) {
+                            callback(response);
+                        }
+
+                    }).
+                    error(function () {
+                        if (isAlert)
+                            _errorAlert();
+                    }).
+                    finally(function () {
+                        if (options.showLoad) {
+                            hideLoading();
+                        }
+
+                        if ((typeof final !== 'undefined') && (typeof final === 'function')) {
+                            final();
+                        }
+                    });
+            }
+        }
+
+        return {
+            showLoading: showLoading,
+            hideLoading: hideLoading,
+            request: request,
+            showAlert: showAlert
+        };
+
+    });
